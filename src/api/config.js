72c3222ecfd2c2b8799763d8d7504fb816e2947f
Anuradha Adhikari <adhikarianuradha336@gmail.com>
2024-11-23 20:04:49 +0530

    added api base url

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 // src/api/config.js
-export const API_BASE_URL = 'http://localhost:3030/api';
+export const API_BASE_URL = 'https://task-management-system-backend-one.vercel.app/api';
 
 export const defaultHeaders = {
     'Content-Type': 'application/json',
