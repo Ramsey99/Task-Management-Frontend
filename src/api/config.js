b7e7e386f70de8b82f135bac1ec8ce69086ee379
Pratanu Ghorui <ghoruipratanu@gmail.com>
2024-11-23 20:12:43 +0530

    modified config

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 // src/api/config.js
 import { getToken } from "../utils/storage";
 const token=getToken();
-export const API_BASE_URL = 'http://localhost:3030/api';
+export const API_BASE_URL = 'https://task-management-system-backend-one.vercel.app/api';
 
 export const defaultHeaders = {
     'Content-Type': 'application/json',
